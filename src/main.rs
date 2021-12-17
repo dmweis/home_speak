c99136571f3a896df42f1be33bc46c2c86d8bb3d
David Weis <davidmichaelweis@gmail.com>
2021-12-17 02:35:03 +0000

    Clippy and fmt

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
 mod speech_service;
 
-use clap::Clap;
-use std::env;
-use rumqtt::{MqttClient, MqttOptions, QoS, ReconnectOptions, Notification};
-use log::*;
-use simplelog::*;
-use std::str;
-use crossbeam_channel::{ unbounded, Sender };
-use warp::Filter;
 use bytes::Bytes;
-
+use clap::Clap;
+use crossbeam_channel::{unbounded, Sender};
+use log::*;
+use rumqtt::{MqttClient, MqttOptions, Notification, QoS, ReconnectOptions};
+use simplelog::*;
+use std::env;
+use std::str;
+use warp::Filter;
 
 fn mqtt_service(sender: Sender<String>) {
-
     let mqtt_options = MqttOptions::new("home_speak", "mqtt.local", 1883)
         .set_reconnect_opts(ReconnectOptions::Always(5));
 
-    let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).expect("Failed to connect to MQTT host");
+    let (mut mqtt_client, notifications) =
+        MqttClient::start(mqtt_options).expect("Failed to connect to MQTT host");
 
-    mqtt_client.subscribe("home/say", QoS::AtLeastOnce).expect("Failed to subscribe to channel");
-    mqtt_client.subscribe("discord/receive/722904321108213871", QoS::AtLeastOnce).expect("Failed to subscribe to channel");
+    mqtt_client
+        .subscribe("home/say", QoS::AtLeastOnce)
+        .expect("Failed to subscribe to channel");
+    mqtt_client
+        .subscribe("discord/receive/722904321108213871", QoS::AtLeastOnce)
+        .expect("Failed to subscribe to channel");
     for notification in notifications {
         match notification {
             Notification::Publish(message) => {
@@ -28,22 +31,25 @@ fn mqtt_service(sender: Sender<String>) {
                     Ok(message_text) => sender.send(message_text.to_owned()).unwrap(),
                     Err(error) => error!("Error while trying to play message {}", error),
                 }
-            },
+            }
             Notification::Disconnection => {
                 warn!("Client lost connection");
-            },
+            }
             Notification::Reconnection => {
                 warn!("client reconnected");
-                mqtt_client.subscribe("home/say", QoS::AtLeastOnce).expect("Failed to subscribe to channel");
-                mqtt_client.subscribe("discord/receive/722904321108213871", QoS::AtLeastOnce).expect("Failed to subscribe to channel");
-            },
+                mqtt_client
+                    .subscribe("home/say", QoS::AtLeastOnce)
+                    .expect("Failed to subscribe to channel");
+                mqtt_client
+                    .subscribe("discord/receive/722904321108213871", QoS::AtLeastOnce)
+                    .expect("Failed to subscribe to channel");
+            }
             other => {
                 warn!("Unexpected message {:?}", other);
             }
         }
     }
 }
-
 
 #[derive(Clap)]
 #[clap(
@@ -52,12 +58,8 @@ fn mqtt_service(sender: Sender<String>) {
     about = "CLI tool for playing text to speech commands using Google text to speech cloud API"
 )]
 struct Opts {
-    #[clap(
-        short = "c",
-        long = "cache-dir",
-        about = "Path to caching directory"
-    )]
-    cache_dir_path: Option<String>
+    #[clap(short = "c", long = "cache-dir", about = "Path to caching directory")]
+    cache_dir_path: Option<String>,
 }
 
 #[tokio::main]
@@ -91,17 +93,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let rest_sender = s.clone();
     let route = warp::path("say")
-    .and(warp::post())
-    .and(warp::body::bytes())
-    .map(move |payload: Bytes| {
-        if let Ok(text) = str::from_utf8(&payload) {
-            rest_sender.send(text.to_owned()).unwrap();
-            "Ok"
-        } else {
-            error!("Failed processing rest request");
-            "Error"
-        }
-    });
+        .and(warp::post())
+        .and(warp::body::bytes())
+        .map(move |payload: Bytes| {
+            if let Ok(text) = str::from_utf8(&payload) {
+                rest_sender.send(text.to_owned()).unwrap();
+                "Ok"
+            } else {
+                error!("Failed processing rest request");
+                "Error"
+            }
+        });
     warp::serve(route).run(([0, 0, 0, 0], 3000)).await;
     Ok(())
 }
--- src/speech_service.rs
+++ src/speech_service.rs
@@ -1,28 +1,24 @@
-use google_tts;
-use std::io::Cursor;
-use rodio::{self, DeviceTrait};
 use log::*;
-use std::path::Path;
+use rodio::{self, DeviceTrait};
+use sha2::{Digest, Sha256};
+use std::fs::{self, File};
 use std::io::prelude::*;
-use sha2::{Sha256, Digest};
-use std::fs::{ File, self };
-use std::io::{ Seek, Read };
+use std::io::Cursor;
+use std::io::{Read, Seek};
+use std::path::Path;
 
 struct AudioCache {
-    cache_dir_path: String
+    cache_dir_path: String,
 }
-
 
 impl AudioCache {
     fn new(cache_dir_path: String) -> Result<AudioCache, Box<dyn std::error::Error>> {
         let path = Path::new(&cache_dir_path);
         fs::create_dir_all(path)?;
         if !path.exists() {
-            Err("Cache dir path doesn't exist")?;
+            return Err("Cache dir path doesn't exist".into());
         }
-        Ok(AudioCache{
-            cache_dir_path: cache_dir_path,
-        })
+        Ok(AudioCache { cache_dir_path })
     }
 
     fn get(&self, text: String) -> Option<Box<impl Read + Seek>> {
@@ -60,16 +56,18 @@ pub struct SpeechService {
 }
 
 impl SpeechService {
-    pub fn new(google_api_key: String, cache_dir_path: Option<String>) -> Result<SpeechService, Box<dyn std::error::Error>> {
+    pub fn new(
+        google_api_key: String,
+        cache_dir_path: Option<String>,
+    ) -> Result<SpeechService, Box<dyn std::error::Error>> {
         let client = google_tts::GoogleTtsClient::new(google_api_key);
 
-        let output_device = rodio::default_output_device().ok_or("Failed to get default output device")?;
+        let output_device =
+            rodio::default_output_device().ok_or("Failed to get default output device")?;
         info!("Started SpeechService with {}", output_device.name()?);
 
         let audio_cache = match cache_dir_path {
-            Some(path) => {
-                Some(AudioCache::new(path)?)
-            },
+            Some(path) => Some(AudioCache::new(path)?),
             None => None,
         };
 
@@ -80,7 +78,10 @@ impl SpeechService {
         })
     }
 
-    fn play<R: Read + Seek + Send + 'static>(&self, data: R) -> Result<(), Box<dyn std::error::Error>> {
+    fn play<R: Read + Seek + Send + 'static>(
+        &self,
+        data: R,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let output_sink = rodio::Sink::new(&self.output_device);
         output_sink.append(rodio::Decoder::new(data)?);
         output_sink.sleep_until_end();
@@ -92,26 +93,33 @@ impl SpeechService {
             if let Some(file) = audio_cache.get(text.clone()) {
                 info!("Using cached value");
                 self.play(file)?;
-                
             } else {
                 info!("Writing new file");
-                let data = self.speech_client.synthesize(
-                    google_tts::TextInput::with_text(text.clone()),
-                    google_tts::VoiceProps::default_english_female_wavenet(),
-                    google_tts::AudioConfig::default_with_encoding(google_tts::AudioEncoding::Mp3)
-                ).await?;
+                let data = self
+                    .speech_client
+                    .synthesize(
+                        google_tts::TextInput::with_text(text.clone()),
+                        google_tts::VoiceProps::default_english_female_wavenet(),
+                        google_tts::AudioConfig::default_with_encoding(
+                            google_tts::AudioEncoding::Mp3,
+                        ),
+                    )
+                    .await?;
                 audio_cache.set(text.clone(), data.as_byte_stream()?)?;
                 let buffer = Cursor::new(data.as_byte_stream()?);
                 self.play(buffer)?;
             }
             Ok(())
         } else {
-            let data = self.speech_client.synthesize(
-                google_tts::TextInput::with_text(text),
-                google_tts::VoiceProps::default_english_female_wavenet(),
-                google_tts::AudioConfig::default_with_encoding(google_tts::AudioEncoding::Mp3)
-            ).await?;
-    
+            let data = self
+                .speech_client
+                .synthesize(
+                    google_tts::TextInput::with_text(text),
+                    google_tts::VoiceProps::default_english_female_wavenet(),
+                    google_tts::AudioConfig::default_with_encoding(google_tts::AudioEncoding::Mp3),
+                )
+                .await?;
+
             let buffer = Cursor::new(data.as_byte_stream()?);
             self.play(buffer)?;
             Ok(())
