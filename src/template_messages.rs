f8670cb19d7c2b6336f3cda0d5793388033dbde9
David Weis <davidmichaelweis@gmail.com>
2022-04-24 07:44:06 +0100

    Reformate template a bit

--- src/template_messages.rs
+++ src/template_messages.rs
@@ -8,23 +8,24 @@ use std::str;
 
 pub fn generate_startup_message() -> String {
     let mut message_buffer = String::new();
-    message_buffer.push_str("Good morning! My name is Joy! ");
+    message_buffer.push_str("Good morning, my name is Joy! ");
     message_buffer.push_str(&human_current_time());
     // TODO(David): Extract this
     // probably use some templating engine too
     if let Ok(network_interfaces) = list_afinet_netifas() {
         if network_interfaces.is_empty() {
             error!("No NICs found");
-            message_buffer.push_str("Huh. It looks like this device has no network interfaces? ")
+            message_buffer.push_str("Huh, It looks like this device has no network interfaces? ")
         } else {
-            message_buffer.push_str("I am detecting the following network interfaces. ");
+            message_buffer.push_str("I am detecting the following network interfaces: ");
             let interface_message: String = network_interfaces
                 .iter()
                 .filter(|(_, ip)| ip.is_ipv4() && !ip.is_loopback())
-                .map(|(name, ip)| format!("{} at {}. ", name, ip))
+                .map(|(name, ip)| format!("{} at {}, ", name, ip))
                 .collect();
             info!("local interfaces are: {:?}", interface_message);
-            message_buffer.push_str(&interface_message);
+            message_buffer.push_str(interface_message.trim_end_matches(", "));
+            message_buffer.push_str(". ");
         }
     } else {
         error!("Failed to query local network interfaces");
@@ -68,7 +69,7 @@ fn human_current_time() -> String {
     let local: chrono::DateTime<chrono::Local> = chrono::Local::now();
 
     format!(
-        "Currently it is {} of {:?}, {} at {}:{:02}. ",
+        "Currently it is {} of {:?}, {} at {}:{:02}, ",
         Ordinal(local.day()),
         Month::from_u32(local.month()).unwrap(),
         local.year(),
