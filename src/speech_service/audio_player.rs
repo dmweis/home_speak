64ec1afa182ef3d11f2758887c352f6b134cb1b3
David Weis <dweis7@gmail.com>
2023-11-23 16:33:55 +0000

    Add automated audio output selection

--- src/speech_service/audio_player.rs
+++ src/speech_service/audio_player.rs
@@ -1,5 +1,6 @@
 use crate::error::{HomeSpeakError, Result};
 use log::*;
+use rodio::cpal::traits::{DeviceTrait, HostTrait};
 use std::io::Seek;
 use std::sync::mpsc::Receiver;
 use std::{
@@ -35,11 +36,43 @@ pub enum AudioPlayerCommand {
     Volume(f32),
 }
 
-fn audio_player_loop(
-    receiver: &Receiver<AudioPlayerCommand>,
-) -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let (_output_stream, output_stream_handle) = rodio::OutputStream::try_default()
-        .map_err(|_| HomeSpeakError::FailedToCreateAnOutputStream)?;
+/// Select the first audio output device that contains "CARD=Device" in its name
+/// This is a hack to select the USB audio device on the raspberry pi
+/// Based on https://github.com/RustAudio/rodio/blob/4973f330e07be8480c35f145c9da84dc60e2184c/src/stream.rs#L57
+fn select_output_device() -> anyhow::Result<(rodio::OutputStream, rodio::OutputStreamHandle)> {
+    let host_ids = rodio::cpal::available_hosts();
+    for host_id in host_ids {
+        let host = rodio::cpal::host_from_id(host_id).unwrap();
+        info!("Found audio host {}", host_id.name());
+        let output_devices = host.devices().unwrap();
+        for device in output_devices {
+            let device_name = device.name().unwrap_or_default();
+            if device_name.contains("CARD=Device") {
+                let default_stream = rodio::OutputStream::try_from_device(&device);
+
+                return Ok(default_stream.or_else(|original_err| {
+                    // default device didn't work, try other ones
+                    let mut devices = match rodio::cpal::default_host().output_devices() {
+                        Ok(d) => d,
+                        Err(_) => return Err(original_err),
+                    };
+
+                    devices
+                        .find_map(|d| rodio::OutputStream::try_from_device(&d).ok())
+                        .ok_or(original_err)
+                })?);
+            }
+        }
+    }
+    anyhow::bail!("No audio output device found");
+}
+
+fn audio_player_loop(receiver: &Receiver<AudioPlayerCommand>) -> anyhow::Result<()> {
+    // let (_output_stream, output_stream_handle) = rodio::OutputStream::try_default()
+    //     .map_err(|_| HomeSpeakError::FailedToCreateAnOutputStream)?;
+
+    let (_output_stream, output_stream_handle) = select_output_device()?;
+
     let sink = rodio::Sink::try_new(&output_stream_handle)
         .map_err(|_| HomeSpeakError::FailedToCreateASink)?;
     loop {
@@ -74,7 +107,7 @@ fn audio_player_loop(
 
 pub fn create_player() -> Sender<AudioPlayerCommand> {
     let (sender, receiver) = channel();
-    thread::spawn(move || loop {
+    thread::spawn(move || {
         // This may miss on sender being dead. But if sender is dead we have bigger issues
         loop {
             if let Err(e) = audio_player_loop(&receiver) {
